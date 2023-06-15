954f75132eb9d6a01ade0d120d975c02a407b172
Anas Taleeb <1203051@student.birzeit.edu>
2023-06-15 20:12:51 +0300

    commit 2

--- app/src/main/java/com/example/pharmacy/Activity/Setting.java
+++ app/src/main/java/com/example/pharmacy/Activity/Setting.java
@@ -1,12 +1,18 @@
 package com.example.pharmacy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.example.pharmacy.R;
+
+import java.util.Objects;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -15,12 +21,51 @@ public class Setting extends AppCompatActivity {
     MeowBottomNavigation bottomNavigation;
     Intent intent;
 
+    Switch nightModeSwitch ;
+    private boolean isUserInteracting = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         getSupportActionBar().hide();
         bottomNavigationSetUp();
+        nightModeSwitch = findViewById(R.id.switch2);
+
+        // Check the current Night Mode state and set the switch accordingly
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        // This change is not due to user interaction
+        isUserInteracting = false;
+
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                nightModeSwitch.setChecked(true);
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                nightModeSwitch.setChecked(false);
+                break;
+        }
+
+        // Allow changes due to user interaction again
+        isUserInteracting = true;
+
+        // Set the listener
+        nightModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isUserInteracting) {
+                    if(isChecked) {
+                        // If the switch is checked, set the Night Mode
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else {
+                        // If the switch is unchecked, set the Light Mode
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+                }
+            }
+        });
     }
 
     private void bottomNavigationSetUp() {
