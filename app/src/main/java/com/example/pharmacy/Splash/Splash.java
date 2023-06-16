10e4dabdeb7343499464638b618a5c00d2fb3fc2
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2023-06-16 23:46:02 +0300

    splash-screen

---
+++ app/src/main/java/com/example/pharmacy/Splash/Splash.java
@@ -0,0 +1,29 @@
+package com.example.pharmacy.Splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pharmacy.Activity.MainActivity;
+import com.example.pharmacy.R;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =  new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
