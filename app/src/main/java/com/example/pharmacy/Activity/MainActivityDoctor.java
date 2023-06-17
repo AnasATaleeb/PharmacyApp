fb6771cd5cfd7b8dcca12ab492f7424c606005b2
Anas Taleeb <1203051@student.birzeit.edu>
2023-06-17 15:16:12 +0300

    Add Doctor Activities

---
+++ app/src/main/java/com/example/pharmacy/Activity/MainActivityDoctor.java
@@ -0,0 +1,178 @@
+package com.example.pharmacy.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.pharmacy.Adaptor.CategoryAdapter;
+import com.example.pharmacy.Adaptor.ItemsAdapter;
+import com.example.pharmacy.Decorator.GridSpacingItemDecoration;
+import com.example.pharmacy.R;
+import com.example.pharmacy.databinding.ActivityMainBinding;
+import com.example.pharmacy.model.Category;
+import com.example.pharmacy.model.Item;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+public class MainActivityDoctor extends AppCompatActivity {
+
+    MeowBottomNavigation bottomNavigation;
+    private ActivityMainBinding binding;
+    private ArrayList<Category> categories;
+    private ArrayList<Item> items;
+    Intent intent;
+
+    TextView allCategory;
+    TextView allitms;
+
+    ShapeableImageView pofile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        allCategory = findViewById(R.id.all_cat);
+        allitms = findViewById(R.id.all_itms);
+        pofile = findViewById(R.id.profile_img);
+
+        pofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivityDoctor.this,Profile.class);
+                startActivity(intent);
+            }
+        });
+
+        allCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivityDoctor.this,Categories.class);
+                startActivity(intent);
+            }
+        });
+
+        allitms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivityDoctor.this,AllItems.class);
+                startActivity(intent);
+            }
+        });
+
+        bottomNavigationSetUp();
+        categoriesSetUp();
+        ItemsSetUp();
+    }
+
+    private void bottomNavigationSetUp() {
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(5, R.drawable.outline_person_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.cart));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.love));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.logistics));
+        bottomNavigation.show(3, true);
+
+        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+
+                switch (model.getId()) {
+                    case 1:
+                        intent = new Intent(MainActivityDoctor.this, Cart.class);
+                        startActivity(intent);
+                        break;
+
+                    case 2:
+                        intent = new Intent(MainActivityDoctor.this, Love.class);
+                        startActivity(intent);
+                        break;
+
+                    case 3:
+                        intent = new Intent(MainActivityDoctor.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 4:
+                        intent = new Intent(MainActivityDoctor.this, Order.class);
+                        startActivity(intent);
+                        break;
+
+                    case 5:
+                        Intent intent = new Intent(MainActivityDoctor.this, Setting.class);
+                        startActivity(intent);
+                        break;
+                }
+                return null;
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+                return null;
+            }
+        });
+    }
+
+    private void categoriesSetUp() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        binding.categoryList.setLayoutManager(linearLayoutManager);
+
+        categories = new ArrayList<>();
+        categories.add(new Category("كل المنتجات", R.drawable.all));
+        categories.add(new Category("أدوية برد", R.drawable.drug));
+        categories.add(new Category("ابر وسرنجات", R.drawable.syringe));
+        categories.add(new Category("عناية بالبشرة", R.drawable.skin));
+        categories.add(new Category("حليب أطفال", R.drawable.milkpowder));
+
+        CategoryAdapter adapter = new CategoryAdapter(this, categories);
+        binding.categoryList.setAdapter(adapter);
+    }
+
+    private void ItemsSetUp() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        binding.itemsList.setLayoutManager(linearLayoutManager);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(
+                R.id.itemsList);
+        RecyclerView.LayoutManager layoutManager ;
+        RecyclerView.Adapter adapter;
+
+        // Set the span count to 2 and create a GridLayoutManager
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Apply the item decoration with the desired spacing
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.spacing);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, true));
+
+        items = new ArrayList<>();
+        items.add(new Item("بانادول" , "لعلاج البرد والرشخ والزكام - 20 قرص",R.drawable.panadolextra ,25.8));
+        items.add(new Item("بانادول" , "لعلاج البرد والرشخ والزكام - 20 قرص",R.drawable.panadolextra ,25.8));
+        items.add(new Item("بانادول" , "لعلاج البرد والرشخ والزكام - 20 قرص",R.drawable.panadolextra ,25.8));
+        items.add(new Item("بانادول" , "لعلاج البرد والرشخ والزكام - 20 قرص",R.drawable.panadolextra ,25.8));
+        items.add(new Item("بانادول" , "لعلاج البرد والرشخ والزكام - 20 قرص",R.drawable.panadolextra ,25.8));
+
+
+        // Set your adapter and data to the RecyclerView
+        adapter = new ItemsAdapter(this,items); // Replace 'YourAdapter' and 'data' with your actual adapter and data
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/pharmacy/Adaptor/ItemsAdapterDoctor.java
@@ -0,0 +1,72 @@
+package com.example.pharmacy.Adaptor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pharmacy.R;
+import com.example.pharmacy.Activity.ViewItem;
+import com.example.pharmacy.databinding.ItemBinding;
+import com.example.pharmacy.model.Item;
+
+import java.util.ArrayList;
+
+
+public class ItemsAdapterDoctor extends RecyclerView.Adapter<ItemsAdapterDoctor.viewHolder> {
+
+    Context context;
+    ArrayList<Item> list;
+
+    public ItemsAdapterDoctor(Context context, ArrayList<Item> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_doctor,parent,false);
+
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        final Item model = list.get(position);
+
+        holder.binding.itemTitle.setText(model.getTitle());
+        Drawable drawable = ContextCompat.getDrawable(context, model.getPic());
+        holder.binding.itemPic.setImageDrawable(drawable);
+        holder.binding.desItem.setText(model.getDiscreption());
+        holder.binding.costItem.setText(model.getPrice()+" شيكل");
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ViewItem.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return  list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        ItemBinding binding;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemBinding.bind(itemView);
+        }
+    }
+}
