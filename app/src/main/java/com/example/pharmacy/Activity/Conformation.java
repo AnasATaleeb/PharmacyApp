2eac73b66d5568839296ee614880d4d67720247a
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2023-06-21 02:08:31 +0300

    shipment-payment-conformation

---
+++ app/src/main/java/com/example/pharmacy/Activity/Conformation.java
@@ -0,0 +1,29 @@
+package com.example.pharmacy.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pharmacy.R;
+
+public class Conformation extends AppCompatActivity {
+private Button btn;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_confirmation);
+
+        // intilize the button
+        btn = findViewById(R.id.btn_next);
+
+        // set on click listener
+        btn.setOnClickListener(v -> {
+            //todo anas
+            Intent intent = new Intent(Conformation.this, MainActivity.class);
+            finish();
+        });
+    }
+}
