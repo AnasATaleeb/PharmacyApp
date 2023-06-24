70a652b27518b6bcf5fc326e6151c0d584ee5c6b
nadeenmoreb02 <132138078+nadeenmoreb02@users.noreply.github.com>
2023-06-24 15:16:20 +0300

    nadeen app sliders

---
+++ app/src/main/java/com/example/pharmacy/Splash/IntroActivity.java
@@ -0,0 +1,112 @@
+package com.example.pharmacy.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.pharmacy.Activity.Login;
+import com.example.pharmacy.R;
+
+public class IntroActivity extends AppCompatActivity {
+    //Variables
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+    Button letsGetStarted;
+    Animation animation;
+    int currentPos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_activity);
+
+        //Hooks
+        viewPager = findViewById(R.id.slider);
+        viewPager.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        dotsLayout = findViewById(R.id.dots);
+        dotsLayout.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        letsGetStarted = findViewById(R.id.get_started_btn);
+
+        //Call adapter
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+
+        //Dots
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    public void skip(View view) {
+        startActivity(new Intent(this, Login.class));
+        finish();
+    }
+
+    public void next(View view) {
+        viewPager.setCurrentItem(currentPos + 1);
+    }
+
+    private void addDots(int position) {
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.green1));
+        }
+
+
+    }
+
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            currentPos = position;
+
+         /*   if (position == 0) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else if (position == 1) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else if (position == 2) {
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            } else {
+                animation = AnimationUtils.loadAnimation(IntroActivity.this, R.anim.fade_in_animation);
+                letsGetStarted.setAnimation(animation);
+                letsGetStarted.setVisibility(View.VISIBLE);
+            }*/
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+}
