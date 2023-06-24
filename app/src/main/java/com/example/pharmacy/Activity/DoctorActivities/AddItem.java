f27172a89135525b0d19c75c4d92fd7ae1c798b2
nadeenmoreb02 <132138078+nadeenmoreb02@users.noreply.github.com>
2023-06-24 21:55:36 +0300

    nadeen app add

--- app/src/main/java/com/example/pharmacy/Activity/DoctorActivities/AddItem.java
+++ app/src/main/java/com/example/pharmacy/Activity/DoctorActivities/AddItem.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 
 import com.example.pharmacy.R;
 
+import java.util.Objects;
+
 public class AddItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        // Hide the action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
