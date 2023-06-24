13770d141e02903d1d7b6a71532cfdf9cb044367
nadeenmoreb02 <132138078+nadeenmoreb02@users.noreply.github.com>
2023-06-24 21:48:12 +0300

    nadeen app search

---
+++ app/src/main/java/com/example/pharmacy/Activity/DoctorActivities/AddItem.java
@@ -0,0 +1,16 @@
+package com.example.pharmacy.Activity.DoctorActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pharmacy.R;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
