aa0ba34f1ba2d2a8894d08d7675d02923967865d
Anas Taleeb <1203051@student.birzeit.edu>
2023-07-06 16:51:25 +0300

    all categories done!

---
+++ app/src/main/java/com/example/pharmacy/Activity/AllCategories.java
@@ -0,0 +1,16 @@
+package com.example.pharmacy.Activity.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pharmacy.R;
+
+public class AllCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_categories);
+    }
+}
